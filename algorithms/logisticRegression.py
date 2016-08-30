2056700d041e1c0f0a72d77d4a61a24b156fac70
Soon Chee Loong <scheeloong@gmail.com>
2016-08-29 20:34:43 -0400

    Added files

---
+++ algorithms/logisticRegression.py
@@ -0,0 +1,25 @@
+class LogisticRegression(object):
+  """Multi-class Logistic Regression Class
+    Classification is done by projecting data points onto a set of hyperplanes, the distance is used to determine
+    a class membership probability. 
+  """
+  def __init__(self, x, n_in, n_out):
+    # Weights
+    self.W = 2
+    # Bias
+    self.B = 1
+
+  def getWeight(self):
+    return self.W
+
+  def setWeight(self, W):
+    self.W = W
+
+  def getBias(self):
+    return self.B
+
+  def setBias(self, B):
+    self.B = B
+
+  def __str__(self):
+    return "Weight: %s Bias: %s" (self.W, self.B)
