65bec6ecdf251a8f51c4974fddbac70b34cf8e90
scheeloong <scheeloong@gmail.com>
2017-02-07 19:56:38 -0500

    Divided error by 2M instead of M

--- library/tensorflow/assignment1.py
+++ library/tensorflow/assignment1.py
@@ -6,6 +6,7 @@
 
 import tensorflow as tf
 import numpy as np
+import sys
 
 # 1.2 Euclidean Distance Function 
 # 1.2.2 Pairwise Distances
@@ -23,9 +24,12 @@ def PairwiseDistances(X, Z):
     B = X.get_shape().as_list()[0]
     N = X.get_shape().as_list()[1]
     C = Z.get_shape().as_list()[0]
+    # Ensure the N dimensions are consistent
     assert  N == Z.get_shape().as_list()[1]
+    # Reshape to make use of broadcasting in Python
     X = tf.reshape(X, [B, 1, N])
     Z = tf.reshape(Z, [1, C, N])
+    # The code below automatically does broadcasting
     D = tf.reduce_sum(tf.square(tf.sub(X, Z)), 2)
     return D
 
@@ -44,6 +48,7 @@ def ChooseNearestNeighbours(D, K):
         topK are the value of the squared distances for the topK
         indices are the index of the location of these squared distances
     """
+    # Take topK of negative distances since it is the closest data.
     topK, indices = tf.nn.top_k(tf.neg(D), K)
     return topK, indices
 
@@ -69,10 +74,11 @@ def PredictKnn(trainData , testData, trainTarget,  testTarget, K):
     # Select the proper outputs to be averaged from the target values and average them
     trainTargetSelectedAveraged = tf.reduce_mean(tf.gather(trainTarget, indices), 1)
     # Calculate the loss from the actual values
-    loss = tf.reduce_mean(tf.square(tf.sub(trainTargetSelectedAveraged, testTarget)))
+    # Divide by 2.0 since it's average over 2M instead of M where M = number of training data.
+    loss = tf.reduce_mean(tf.square(tf.sub(trainTargetSelectedAveraged, testTarget)))/2.0
     return loss
 
-# Plot the prediction function for x = [0, 11]
+# Plot the prediction function for x = [0, 11] on training data.
 def PredictedValues(x, trainData, trainTarget, K):
     """
     Plot the predicted values
@@ -84,17 +90,42 @@ def PredictedValues(x, trainData, trainTarget, K):
     predictedValues = tf.reduce_mean(tf.gather(trainTarget, indices), 1)
     return predictedValues
 
+# 1.4 Soft-Knn & Gaussian Processes
+# 1.4.1.1 Soft Decisions
+# TODO: Write a Tensorflow python program based on the soft k-NN model to compute 
+# predictions on the data1D.npy dataset. 
+# Set lambda = 0.1 NOT 10 as given in assignment handout
+# and plot the test-set prediction of the model. 
+
+def SoftDecisions(hyperParameter = 0.1): 
+    # K = exponentiated negative pairwise distances
+    # K = exp(-hyperParameter*PairwiseDistances(testData, trainData))
+    # r = K/(tf.reducesumK(x,x) # Sum it downwards, should be a scalar and normalizing constant.
+    r = 0 #temp
+    return r
+
+def GaussianProcessRegression(hyperParameter = 0.1):
+    # k1 = exp(-hyperparameter*pairwisedistances(testdata, traindata))
+    # k2 = exp(-hyperparameter*pairwisedistances(traindata, traindata))
+    # r = tf.inverse(k2)matmultiply(k1)
+    r = 0 #temp
+    return r
+
+# 1.4.1.1 Gaussian Processes
+# TODO: Repeat for the Gaussian Processes Regression Model
+# Comment on the difference you observe between two programs
+
 # 2 Linear and Logistic Regression
 # 2.2 Stochastic Gradient Descent
 # Implement linear regression and stochastic gradient descent algorithm 
 # with mini-batch size B = 50.
-def buildGraph():
+def buildGraph(learningRate, weightDecayCoeff):
     # Variable creation
     W = tf.Variable(tf.truncated_normal(shape=[64, 1], stddev=0.5), name='weights')
     b = tf.Variable(0.0, name='biases')
     X = tf.placeholder(tf.float32, [None, 64], name='input_x')
     y_target = tf.placeholder(tf.float32, [None,1], name='target_y')
-    weightDecay = tf.div(tf.constant(1.0),tf.constant(2.0))
+    weightDecay = tf.div(tf.constant(weightDecayCoeff),tf.constant(2.0))
     # Graph definition
     y_predicted = tf.matmul(X,W) + b 
     # Error definition
@@ -107,7 +138,7 @@ def buildGraph():
     meanSquaredError = tf.add(meanSquaredError,weightDecayTerm)
 
     # Training mechanism
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate = learningRate)
     train = optimizer.minimize(loss=meanSquaredError)
     return W, b, X, y_target, y_predicted, meanSquaredError, train
 
@@ -117,8 +148,14 @@ def ShuffleBatches(trainData, trainTarget):
     return trainData, trainTarget
 
 def LinearRegression(trainData, trainTarget, testData, testTarget):
+    #TODO: For loop across different learning rates.
+    learningRateTrials = [0.0, 0.0001, 0.0001, 0.01, 0.1, 1.0]
+    miniBatchSizeTrials = [10, 50, 100, 700]
+    learningRate = 0.01
+    miniBatchSize = 10
+    weightDecayCoeff = 1.0
     # Build computation graph
-    W, b, X, y_target, y_predicted, meanSquaredError, train = buildGraph()
+    W, b, X, y_target, y_predicted, meanSquaredError, train = buildGraph(learningRate, weightDecayCoeff)
     # Initialize session
     init = tf.global_variables_initializer()
     sess = tf.InteractiveSession()
@@ -131,11 +168,12 @@ def LinearRegression(trainData, trainTarget, testData, testTarget):
     wList = []
 
     numEpoch = 20
-    miniBatchSize = 10
     currEpoch = 0
     xAxis = []
     yAxis = []
     numUpdate = 0
+    step = 1
+    err = -1 
     while currEpoch <= numEpoch:
         # Shuffle the batches and return
         trainData, trainTarget = ShuffleBatches(trainData, trainTarget)
@@ -144,8 +182,8 @@ def LinearRegression(trainData, trainTarget, testData, testTarget):
         while step*miniBatchSize < 700:
             _, err, currentW, currentb, yhat = sess.run([train, meanSquaredError, W, b, y_predicted], feed_dict={X: trainData[step*miniBatchSize:(step+1)*miniBatchSize], y_target: trainTarget[step*miniBatchSize:(step+1)*miniBatchSize]})
             wList.append(currentW)
-            if not (step*miniBatchSize % 50):
-                print "Iter: %3d, MSE-train: %4.2f, weights: %s, bias: %.2f", step, err, currentW.T, currentb
+            #if not (step*miniBatchSize % 50):
+            #    print "Iter: %3d, MSE-train: %4.2f, weights: %s, bias: %.2f", step, err, currentW.T, currentb
             step = step + 1
             xAxis.append(numUpdate)
             numUpdate += 1
@@ -154,11 +192,9 @@ def LinearRegression(trainData, trainTarget, testData, testTarget):
         errTest = sess.run(meanSquaredError, feed_dict={X: testData, y_target: testTarget})
         print "Final testing MSE: ", errTest
         currEpoch += 1
+    print "Iter: ", step, "MSE-train:", err
     import matplotlib.pyplot as plt
     plt.figure(1)
-    print "Axes are"
-    print np.array(xAxis)
-    print np.array(yAxis)
     plt.plot(np.array(xAxis), np.array(yAxis))
     plt.savefig('trainEpoch.png')
     return
@@ -181,42 +217,50 @@ if __name__ == "__main__":
     # and existing training input
     topK, indices = ChooseNearestNeighbours(D, K)
     # Prediction
-    np.random.seed(521)
-    Data = np.linspace(1.0 , 10.0 , num =100) [:, np.newaxis]
-    Target = np.sin( Data ) + 0.1 * np.power( Data , 2) + 0.5 * np.random.randn(100 , 1)
-    randIdx = np.arange(100)
-    np.random.shuffle(randIdx)
-    trainData, trainTarget  = Data[randIdx[:80]], Target[randIdx[:80]]
-    validData, validTarget = Data[randIdx[80:90]], Target[randIdx[80:90]]
-    testData, testTarget = Data[randIdx[90:100]], Target[randIdx[90:100]]
-    # Convert to tensors from numpy
-    trainData = tf.pack(trainData)
-    validData = tf.pack(validData)
-    testData = tf.pack(testData)
-    trainTarget = tf.pack(trainTarget)
-    validtarget = tf.pack(validTarget)
-    testTarget = tf.pack(testTarget)
-    trainMseLoss = PredictKnn(trainData, trainData, trainTarget, trainTarget, K)
-    validationMseLoss = PredictKnn(trainData, validData, trainTarget, validTarget, K)
-    testMseLoss = PredictKnn(trainData, testData, trainTarget, testTarget, K)
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-        print sess.run(trainMseLoss)
-        print sess.run(validationMseLoss)
-        print sess.run(testMseLoss)
-    # Plot the prediction for the x below
-    x = np.linspace(0.0, 11.0, num=1000)[:, np.newaxis]
-    xTensor = tf.pack(x)
-    predictedValues = PredictedValues(xTensor, trainData, trainTarget, K)
-    import matplotlib.pyplot as plt
-    plt.figure(0)
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-        plt.scatter(sess.run(trainData), sess.run(trainTarget))
-        plt.plot(sess.run(xTensor), sess.run(predictedValues))
-        plt.savefig('haha.png')
+    for K in [1, 3, 5, 50]:
+        np.random.seed(521)
+        Data = np.linspace(1.0 , 10.0 , num =100) [:, np.newaxis]
+        Target = np.sin( Data ) + 0.1 * np.power( Data , 2) + 0.5 * np.random.randn(100 , 1)
+        randIdx = np.arange(100)
+        np.random.shuffle(randIdx)
+        trainData, trainTarget  = Data[randIdx[:80]], Target[randIdx[:80]]
+        validData, validTarget = Data[randIdx[80:90]], Target[randIdx[80:90]]
+        testData, testTarget = Data[randIdx[90:100]], Target[randIdx[90:100]]
+        # Convert to tensors from numpy
+        trainData = tf.pack(trainData)
+        validData = tf.pack(validData)
+        testData = tf.pack(testData)
+        trainTarget = tf.pack(trainTarget)
+        validtarget = tf.pack(validTarget)
+        testTarget = tf.pack(testTarget)
+        trainMseLoss = PredictKnn(trainData, trainData, trainTarget, trainTarget, K)
+        validationMseLoss = PredictKnn(trainData, validData, trainTarget, validTarget, K)
+        testMseLoss = PredictKnn(trainData, testData, trainTarget, testTarget, K)
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init)
+            print 'K'
+            print K
+            print 'trainMseLoss'
+            print sess.run(trainMseLoss)
+            print 'validationMseLoss'
+            print sess.run(validationMseLoss)
+            print 'testMseLoss'
+            print sess.run(testMseLoss)
+        # Plot the prediction for the x below
+        x = np.linspace(0.0, 11.0, num=1000)[:, np.newaxis]
+        xTensor = tf.pack(x)
+        predictedValues = PredictedValues(xTensor, trainData, trainTarget, K)
+        import matplotlib.pyplot as plt
+        plt.figure(0)
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init)
+            plt.figure(K+100)
+            plt.scatter(sess.run(trainData), sess.run(trainTarget))
+            plt.plot(sess.run(xTensor), sess.run(predictedValues))
+            fileName = str("KNN") + str(K) + str("trainingGraph.png")
+            plt.savefig(fileName)
     # Part 2
     with np.load ("tinymnist.npz") as data :
         trainData, trainTarget = data ["x"], data["y"]
@@ -225,14 +269,6 @@ if __name__ == "__main__":
         LinearRegression(trainData, trainTarget, testData, testTarget)
 '''
 
-# 1.4 Soft-Knn & Gaussian Processes
-# 1.4.1.1 Soft Decisions
-# TODO: Write a Tensorflow python program based on the soft k-NN model to compute 
-# predictions on the data1D.npy dataset. 
-# Set lambda = 1 and plot the test-set prediction of the model. 
-# 1.4.1.1 Gaussian Processes
-# TODO: Repeat for the Gaussian Processes Regression Model
-# Comment on the difference you observe between two programs
 
 # 2.2.1 Tuning the learning rate
 # Train the linear regression model on the tiny MNIST dataset using SGD
