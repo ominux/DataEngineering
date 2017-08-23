14087a4c0bd93c6c994714b1e3919468b956fc83
scheeloong <scheeloong@gmail.com>
2017-08-23 04:36:16 -0400

    .

--- DataEngineering/algorithm/test_kNearestNeighbour.py
+++ DataEngineering/algorithm/test_kNearestNeighbour.py
@@ -14,9 +14,11 @@ class TestKNearestNeighbour(unittest.TestCase):
     def testDistanceMatrix(self):
         trainData = np.array([[1.0, 2.0], [3.0, 4.0]])
         trainLabel = np.array([0, 1])
-        print("TrainDataShape", trainData.shape)
-        print("TrainLabel", trainLabel.shape)
         knn = KNearestNeighbour(trainData, trainLabel)
+        testData = trainData
+        distanceMatrix = knn.computeDistanceMatrix(testData)
+        # Ensure that the diagonal elements are all 0
+        assert np.sum(distanceMatrix.diagonal()) == 0.0
 
     @classmethod
     def tearDownClass(self):
