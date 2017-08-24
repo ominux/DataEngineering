6e49ab77f957336a5b63778197d13f2c60726596
scheeloong <scheeloong@gmail.com>
2017-08-24 05:08:05 -0400

    .

--- DataEngineering/algorithm/__init__.py
+++ DataEngineering/algorithm/__init__.py
@@ -2,6 +2,7 @@
 from convolutionalNeuralNetwork import ConvolutionalNeuralNetwork
 from logisticRegression import LogisticRegression
 from kNearestNeighbour import KNearestNeighbour
+from linearSupportVectorMachine import LinearSupportVectorMachine
 
 # Thus, the filename isn't seen by the user, only the class name is
 # as long as you add the lines above to your __init__.py file
---
+++ DataEngineering/algorithm/linearSupportVectorMachine.py
@@ -0,0 +1,5 @@
+# TODO: 
+
+class LinearSupportVectorMachine(object):
+    def __init__(self):
+        pass
