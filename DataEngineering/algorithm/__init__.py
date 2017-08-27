80a30acf583e0482afce9b86d9457f94a92d95c2
scheeloong <scheeloong@gmail.com>
2017-08-27 05:11:22 -0400

    Added fully connected neural network as TODO

--- DataEngineering/algorithm/__init__.py
+++ DataEngineering/algorithm/__init__.py
@@ -1,9 +1,10 @@
 # from currFilename.py import classNameThatExistInCurrentFileName.py
-from convolutionalNeuralNetwork import ConvolutionalNeuralNetwork
 from logisticRegression import LogisticRegression
 from kNearestNeighbour import KNearestNeighbour
 from linearSupportVectorMachine import LinearSupportVectorMachine
 from linearSoftmax import LinearSoftmax
+from fullyConnectedNeuralNetwork import FullyConnectedNeuralNetwork
+from convolutionalNeuralNetwork import ConvolutionalNeuralNetwork
 
 # Thus, the filename isn't seen by the user, only the class name is
 # as long as you add the lines above to your __init__.py file
---
+++ DataEngineering/algorithm/fullyConnectedNeuralNetwork.py
@@ -0,0 +1,7 @@
+class FullyConnectedNeuralNetwork(object):
+    """
+    A Fully Connected Neural Network
+    """
+    def __init__(self):
+        # TODO
+        pass
